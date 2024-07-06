a632aecb977e74066944d4abfb1bdbdf7cb8d22d
rahul-yadav20 <rahul.ydv0927@gmail.com>
2024-07-06 18:26:57 +0530

    Changing Overview Hover Effect

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,10 @@ const MovieCard = ({ movie }) => {
       <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p className="movie-overview">{movie.overview}</p>
+        <div className="movie-overview">
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+        </div>
         <p>Genres: {movie.genres.map(genre => genre.name).join(', ')}</p>
         <p>Cast: {movie.cast.join(', ')}</p>
         <p>Director: {movie.director}</p>
