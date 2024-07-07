ce8112599f1b6dade52fe392ac00e7bc8e181ab0
rahul-yadav20 <rahul.ydv0927@gmail.com>
2024-07-07 18:51:56 +0530

    Debounce Comments

--- src/hooks/useDebounce.jsx
+++ src/hooks/useDebounce.jsx
@@ -1,14 +1,19 @@
-// src/hooks/useDebounce.js
+// Hooks 
 import { useState, useEffect } from 'react';
 
+// Custom Hook for Debouncing
 const useDebounce = (value, delay) => {
+  // State Variable to store search box value
   const [debouncedValue, setDebouncedValue] = useState(value);
 
+  //  useEffect run whenever value or delay time change
   useEffect(() => {
+    // setTimeout take few seconds to set the value for state variable
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
 
+    // Clear timeout effect after delay
     return () => {
       clearTimeout(handler);
     };
